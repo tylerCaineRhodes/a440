abc2dfd7869e0aecaa5571db68b012d8d5eebbfb
Tyler Rhodes <rhodetyl000@gmail.com>
2022-10-23 09:55:10 -0500

    Revert "Support mobile too"
    This reverts commit 374fa3898ded527694d88264fa2ce7965edb2072.

--- client/main.js
+++ client/main.js
@@ -25,15 +25,5 @@ class A440 {
   }
 }
 
-const isTouchDevice = () => {
-  return (
-    'ontouchstart' in window ||
-    navigator.maxTouchPoints > 0 ||
-    navigator.msMaxTouchPoints > 0
-  );
-};
-const listenEvent = isTouchDevice() ? 'touchstart' : 'click';
 const a440 = new A440();
-document.addEventListener(listenEvent, () =>
-  a440.toggle()
-);
+document.addEventListener('click', () => a440.toggle());
