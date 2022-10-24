c5f58a3902a3f2a76133de972d393a4d9b28ae98
Tyler Rhodes <rhodetyl000@gmail.com>
2022-10-24 08:26:05 -0500

    Don't load audio context too soon

--- client/main.js
+++ client/main.js
@@ -1,5 +1,4 @@
-const context = new window.AudioContext() || new window.webkitAudioContext();
-
+const button = document.querySelector('button');
 class A440 {
   constructor() {
     this.isPlaying = false;
@@ -9,21 +8,26 @@ class A440 {
   }
   #pause() {
     this.oscillator.stop();
-    this.oscillator.disconnect(context.destination);
+    button.style.backgroundColor = '#f0f0f0';
+    this.oscillator.disconnect(this.context.destination);
     this.isPlaying = false;
   }
   #play() {
     this.oscillator = this.#createOscillator();
     this.oscillator.start();
+    button.style.backgroundColor = '#d4d4d6';
     this.isPlaying = true;
   }
   #createOscillator() {
+    this.context ||=
+      new window.AudioContext() || new window.webkitAudioContext();
+
+    const { context } = this;
     const osc = context.createOscillator();
     osc.frequency.value = 440;
     osc.connect(context.destination);
     return osc;
   }
 }
-
 const a440 = new A440();
-document.addEventListener('click', () => a440.toggle());
+button.addEventListener('click', () => a440.toggle());
